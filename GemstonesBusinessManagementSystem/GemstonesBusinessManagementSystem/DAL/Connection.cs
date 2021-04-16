75b31cdfee7370c7947041dbd94240bc39a27ba6
Trung Huỳnh <57293778+trunghuynh2304@users.noreply.github.com>
2021-04-16 09:39:54 +0700

    Add resources
    - Thêm các resources và styles
    - Mainwindow
    - Employee Control

---
+++ GemstonesBusinessManagementSystem/GemstonesBusinessManagementSystem/DAL/Connection.cs
@@ -0,0 +1,21 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GemstonesBusinessManagementSystem.DAL
+{
+    public class Connection
+    {
+        private string strCon;
+        public MySqlConnection conn;
+
+        public Connection()
+        {
+            strCon = "server=localhost;user id=root;persistsecurityinfo=False;database=gemstonesbusinessmanagementsystem";
+            conn = new MySqlConnection(strCon);
+        }
+    }
+}
